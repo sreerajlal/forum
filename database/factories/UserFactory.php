79833bdf9194decb595967720783616ac8bb044c
= <=>
2018-09-04 23:44:25 +0530

    Recrate forum to chpater 11

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,3 +21,33 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+$factory->define(App\Thread::class, function ($faker) {
+    return [
+        'user_id' => function () {
+            return factory('App\User')->create()->id;
+        },
+        'channel_id' => function () {
+            return factory('App\Channel')->create()->id;
+        },
+        'title' => $faker->sentence,
+        'body'  => $faker->paragraph
+    ];
+});
+$factory->define(App\Channel::class, function ($faker) {
+    $name = $faker->word;
+    return [
+        'name' => $name,
+        'slug' => $name
+    ];
+});
+$factory->define(App\Reply::class, function ($faker) {
+    return [
+        'thread_id' => function () {
+            return factory('App\Thread')->create()->id;
+        },
+        'user_id' => function () {
+            return factory('App\User')->create()->id;
+        },
+        'body'  => $faker->paragraph
+    ];
+});
---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace Tests\Feature;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+class ReadThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+    protected $thread;
+    public function setUp()
+    {
+        parent::setUp();
+        $this->thread = create('App\Thread');
+    }
+    /** @test */
+    public function a_user_can_view_all_threads()
+    {
+        $this->get('/threads')
+            ->assertSee($this->thread->title);
+    }
+    /** @test */
+    function a_user_can_read_a_single_thread()
+    {
+        $this->get($this->thread->path())
+            ->assertSee($this->thread->title);
+    }
+    /** @test */
+    function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        $reply = create('App\Reply', ['thread_id' => $this->thread->id]);
+        $this->get($this->thread->path())
+            ->assertSee($reply->body);
+    }
+    /** @test */
+    function a_user_can_filter_threads_according_to_a_channel()
+    {
+        $channel = create('App\Channel');
+        $threadInChannel = create('App\Thread', ['channel_id' => $channel->id]);
+        $threadNotInChannel = create('App\Thread');
+        $this->get('/threads/' . $channel->slug)
+            ->assertSee($threadInChannel->title)
+            ->assertDontSee($threadNotInChannel->title);
+    }
+}
