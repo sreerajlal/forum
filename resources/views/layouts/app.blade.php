d6b27e65deb482b43154d800d59493b8c1eff783
= <=>
2018-09-05 19:44:18 +0530

    chapter 11 test fail fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,15 +36,15 @@
                     <ul class="navbar-nav mr-auto">
 
 
-                        <li><a href="/threads">All Threads</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/threads">All Threads</a></li>
 
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                            <a class="nav-link" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                                aria-expanded="false">Channels <span class="caret"></span></a>
 
                             <ul class="dropdown-menu">
                                 @foreach (App\Channel::all() as $channel)
-                                    <li><a href="/threads/{{ $channel->slug }}">{{ $channel->name }}</a></li>
+                                    <li><a class="nav-link" href="/threads/{{ $channel->slug }}">{{ $channel->name }}</a></li>
                                 @endforeach
                             </ul>
                         </li>
--- tests/Feature/ParticipateInThreadsTest.php
+++ tests/Feature/ParticipateInThreadsTest.php
@@ -6,12 +6,15 @@ class ParticipateInThreadsTest extends TestCase
 {
     use DatabaseMigrations;
     /** @test */
-//    function unauthenticated_users_may_not_add_replies()
-//    {
-//        $this->withExceptionHandling()
-//            ->post('/threads/some-channel/1/replies', [])
-//            ->assertRedirect('/login');
-//    }
+    function unauthenticated_users_may_not_add_replies()
+    {
+
+        $this->withExceptionHandling();
+        $thread = create('App\Thread');
+         $this->post($thread->path() . '/replies', [])
+            ->assertRedirect('/login');
+    }
+
     /** @test */
     function an_authenticated_user_may_participate_in_forum_threads()
     {
