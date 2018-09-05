04b659df785aaf696f409e4fe51ecf1fa488b69b
= <=>
2018-09-06 00:06:06 +0530

    chapter 13 completed

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -21,7 +21,7 @@
                                 <label for="channel"></label>
                                 <select class="form-control"  name="channel_id" id="">
                                     <option value="">Choose one..</option>
-                                    @foreach (App\Channel::all() as $channel)
+                                    @foreach ($channels as $channel)
                                         <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected':'' }}>
                                             {{ $channel->name }}
                                         </option>
