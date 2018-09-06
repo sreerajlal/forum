f6f987b0cd31d1074e8dd12e2ec0e6b664488135
= <=>
2018-09-06 15:02:00 +0530

    Chapter 15 push all methords to an abract class

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -3,28 +3,9 @@
 namespace App\Filters;
 
 use App\User;
-use Illuminate\Http\Request;
 
 
-class ThreadFilters{
-
-    protected $request;
-    protected $builder;
-
-    public function __construct(Request $request)
-    {
-        $this->request = $request;
-    }
-
-    public function apply($builder)
-    {
-        $this->builder = $builder;
-
-        if ($this->request->has('by')){
-            $this->by($this->request->by);
-        }
-         return $this->builder;
-    }
+class ThreadFilters extends Filters {
 
     /**
      * @param $userName
